cfe686ffc855ff8af49fe7525d4823c278a1fcb1
samet tunay <samet.tunay12@gmail.com>
2022-10-26 17:17:49 +0300

    update

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -19,6 +21,7 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
             if (car.Description.Length < 10)
